7629d6852e84c625ee0090aa215572f7fd5ca79c
Nwet Nwet Wai <nwetnwet.wai@frontiir.net>
2024-11-26 00:11:37 +0630

    clean code

--- data/src/main/kotlin/com/hana/data/database/AppDatabase.kt
+++ data/src/main/kotlin/com/hana/data/database/AppDatabase.kt
@@ -7,14 +7,10 @@ import com.hana.data.database.dao.CustomerDao
 import com.hana.data.database.entity.CustomerEntity
 
 @Database(
-//    entities = [CustomerEntity::class, AddressEntity::class, GeoEntity::class, CompanyEntity::class],
     entities = [CustomerEntity::class],
     version = 1
 )
 
 abstract class AppDatabase : RoomDatabase() {
     abstract fun customerDao(): CustomerDao
-//    abstract fun addressDao(): AddressDao
-//    abstract fun geoDao(): GeoDao
-//    abstract fun companyDao(): CompanyDao
 }
--- data/src/main/kotlin/com/hana/data/repository/CustomerRepositoryImpl.kt
+++ data/src/main/kotlin/com/hana/data/repository/CustomerRepositoryImpl.kt
@@ -13,41 +13,9 @@ class CustomerRepositoryImpl(
     private val customerDao: CustomerDao,
 ) : CustomerRepository {
 
-
-//    override suspend fun getCustomerList(): RepoResult<List<Customer>> {
-//        // Fetch all customers with address and geo details from the database
-//        val customers = customerDao.getAllCustomers()
-//        println("DATA:::$customers")
-//
-//        // Map geo for each customer (assuming you want to get geo for each individual address)
-//        val geoList = customers.map { it.addressWithGeo?.geo?.toDomain()   }
-//        println("Geo Data:::$geoList")
-//
-//        // Map address and company to domain models for each customer
-//        val customerList = customers.mapIndexed { index, customer ->
-//            val address = customer.addressWithGeo?.address?.toDomain(geoList[index])  // Handle nullability
-//            val company = customer.company?.toDomain()  // Assuming company is non-null
-//            customer.customer.toDomain(address, company)
-////            val address = customer.addressWithGeo?.address?.toDomain(geoList[index]) // Transform address with geo
-////            val company = customer.company.toDomain() // Assuming company also has a `toDomain()` function
-////            customer.customer.toDomain(address, company) // Transform the whole CustomerEntity to Customer
-//        }
-//        println("Customer Data:::$customerList")
-
-        // Return the transformed list of domain models
-//        return RepoResult.Success(customerList)
-//    }
-
-
-
     suspend fun syncData() {
         val apiData = apiService.fetchCustomerData()
         apiData.forEach { customerDao.insertAll(it.toEntity()) }
-
-//        val entities = apiData.map { it.toEntity(it.address?.id ?: 0, it.company?.id ?:0) }
-//       entities.forEach {  customerDao.insertCustomer(it) }
-
-
     }
 
     override suspend fun getCustomerList(): RepoResult<List<Customer>> {
